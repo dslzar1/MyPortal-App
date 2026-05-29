2174d051a5fe95ec547a17b4f19dc14155137f74
Daennah <daennahs@gmail.com>
2026-05-28 17:58:23 -0700

    Fixed table error

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE students {
-    id SERIAL PRIMARY KEY
-    name VARCHAR(100)
-    email VARCHAR(100)
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
     grade_level INT
-};
+);
