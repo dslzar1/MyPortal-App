22868f40800f9beb249ed5e94df9bb04e04e306e
Raphael <raphaelm@uvic.ca>
2026-05-28 18:51:23 -0700

    updated teacher table after merge conf

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,12 @@
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),g
+    name VARCHAR(100),
     email VARCHAR(100),
     grade_level INT
 );
+
+
+CREATE TABLE teachers (
+    name VARCHAR(100),
+    email VARCHAR(100)
+)
