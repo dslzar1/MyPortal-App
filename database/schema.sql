4ee49ba411a9ac4257df171a5101db7d1c320e46
Daennah <daennahs@gmail.com>
2026-05-28 18:41:22 -0700

    enrolments

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,12 @@
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
+    name VARCHAR(100),g
     email VARCHAR(100),
     grade_level INT
-);
+)
+
+
+CREATE TABLE enrollments (
+    student_id INT REFERENCES students(id)
+    class_id INT REFERENCES classes(id)
+)
